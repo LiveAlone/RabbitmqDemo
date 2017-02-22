0bcc0ac2c4346a2494d38270b18d83f7426e16c3
yaoqijun <yaoqijunmail@foxmail.com>
2017-02-22 22:07:58 +0800

    add all

---
+++ src/main/java/org/yqj/rabbitmq/demo/document/MainClass.java
@@ -0,0 +1,13 @@
+package org.yqj.rabbitmq.demo.document;
+
+/**
+ * Created by yaoqijun.
+ * Date:2016-10-27
+ * Email:yaoqijunmail@gmail.io
+ * Descirbe:
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("this is basic test document in rabbitmq client");
+    }
+}
---
+++ src/main/java/org/yqj/rabbitmq/demo/helloworld/Send.java
@@ -0,0 +1,37 @@
+package org.yqj.rabbitmq.demo.helloworld;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+/**
+ * Created by yaoqijun.
+ * Date:2016-10-26
+ * Email:yaoqijunmail@gmail.io
+ * Descirbe:
+ */
+public class Send {
+
+    private static final String QUEUE_NAME = "hello";
+
+    public static void main(String[] args) throws Exception{
+
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("localhost");
+        Connection connection = connectionFactory.newConnection();
+        Channel channel = connection.createChannel();
+
+        // create queue
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+        // send message
+        String message = "hello world";
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+        System.out.println("finish to publish message : " + message);
+
+        // close
+        channel.close();
+        connection.close();
+    }
+
+}
